cd5a52e825722a8382954371c33730af6d2a287f
José Ramón Balsas <jrbalsas@ujaen.es>
2018-04-15 14:16:24 +0200

    ClientesRouteCtrl migrated to ES6 class
    - updated angular version

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -8,10 +8,10 @@ angular.module('clientesApp', [
   'clientesApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/lista', {templateUrl: 'partials/clientes/lista.html', controller: 'ClientesRouteCtrl as cliRtCtrl'});
-  $routeProvider.when('/visualiza/:idCliente', {templateUrl: 'partials/clientes/visualiza.html', controller: 'ClientesRouteCtrl as cliRtCtrl'});
-  $routeProvider.when('/borra/:idCliente', {templateUrl: 'partials/clientes/lista.html', controller: 'ClientesRouteCtrl as cliRtCtrl'});
-  $routeProvider.when('/edita/:idCliente', {templateUrl: 'partials/clientes/edita.html', controller: 'ClientesRouteCtrl as cliRtCtrl'});
-  $routeProvider.when('/crea', {templateUrl: 'partials/clientes/edita.html', controller: 'ClientesRouteCtrl as cliRtCtrl'});
+  $routeProvider.when('/lista', {templateUrl: 'partials/clientes/lista.html', controller: 'ClientesRouteCtrl', controllerAs: '$ctrl' });
+  $routeProvider.when('/visualiza/:idCliente', {templateUrl: 'partials/clientes/visualiza.html', controller: 'ClientesRouteCtrl', controllerAs: '$ctrl'});
+  $routeProvider.when('/borra/:idCliente', {templateUrl: 'partials/clientes/lista.html', controller: 'ClientesRouteCtrl', controllerAs: '$ctrl'});
+  $routeProvider.when('/edita/:idCliente', {templateUrl: 'partials/clientes/edita.html', controller: 'ClientesRouteCtrl', controllerAs: '$ctrl'});
+  $routeProvider.when('/crea', {templateUrl: 'partials/clientes/edita.html', controller: 'ClientesRouteCtrl', controllerAs: '$ctrl'});
   $routeProvider.otherwise({redirectTo: '/lista'});
 }]);
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -5,15 +5,19 @@ class ClientesCtrl {
 
     constructor (clientesDAO) {
         this.clientesDAO=clientesDAO;
-        //view model
+    }
+    
+    $onInit () {
+        //Init view model
         this.editMode=false;
         this.cliente={};
         this.clientes=[];
         this.errMsgs=[];
-        //Init controller
+        //Init view
         this.updateClientes();
-        this.reset();
+        this.reset();        
     }
+    
     //view actions
     crea () {
         this.reset();
@@ -70,73 +74,89 @@ class ClientesCtrl {
   
 //ClientesCtrl.$inject = ['ClientesDAOREST'];
 
-angular.module('clientesApp.controllers', [])
-  .controller('ClientesCtrl', ['ClientesDAOREST', ClientesCtrl ] )
-  .controller('ClientesRouteCtrl', ['$scope','$routeParams','$location','ClientesDAOREST',function($scope,$routeParams,$location,clientesDAO) {
-    //ClientesCtrl routing action version
+class ClientesRouteCtrl {
+    constructor ($routeParams,$location,clientesDAO) {
+        //Dependency injection
+        this.$routeParams=$routeParams;
+        this.$location=$location;
+        this.clientesDAO=clientesDAO;
+        
+        this.$onInit();
 
-    //used to access controller when some method is used as callback on other object
-    var self=this;
-    
-    //view model 
-    this.cliente={};
-    this.clientes=[];
-    this.errMsgs=[];
+    }
 
     //edita.html view button actions
-    this.borra=function (id) {
+    borra (id) {
         if (angular.isNumber(id)) {
-            clientesDAO.borra(id).then(this.updateClientes).catch(this.errorDAO);
+            this.clientesDAO.borra(id)
+                    .then(  () => this.updateClientes() )
+                    .catch( response => this.errorDAO(response) );
         };
-    };
-    this.guarda=function (cliente) {
+    }
+    guarda (cliente) {
       if (cliente.id>0) {
         //Modify cliente data
-        clientesDAO.guarda(cliente).then(this.updateClientes).catch(this.errorDAO);
+        this.clientesDAO.guarda(cliente)
+                    .then(  () => this.updateClientes() )
+                    .catch( response => this.errorDAO(response) );
       } else {
         //New cliente
         cliente.id=0;
-        clientesDAO.crea(cliente).then(this.updateClientes).catch(this.errorDAO);
+        this.clientesDAO.crea(cliente)
+                    .then(  () => this.updateClientes() )
+                    .catch( response => this.errorDAO(response) );
       }
-    };
-
-    this.updateClientes= function () {
-        $location.path("/lista");  //Post-Redirect-Get
-    };
-
-    this.errorDAO= function (response) {
-        self.errMsgs= response.data; //JAX-RS BeanValidation errors
-        console.log( "Error en servidor: " + response.status +" "+ response.statusText );
-    };
-
-    //Process path actions
-    var idCliente=($routeParams.idCliente?parseInt($routeParams.idCliente):0); 
-    var action="";    
-    if ($location.path().length) {
-        //extract action name from path
-        action=$location.path().match(/^\/?(\w+)/)[1];
     }
 
-    switch(action) {
-        case "visualiza":
-        case "edita":
-            clientesDAO.busca(idCliente).then(function (cliente) {           
-                self.cliente=cliente;
-            }).catch(this.errorDAO);
-            break;
-        case "crea":
-            this.cliente={};
-            break;
-        case "borra":
-            clientesDAO.borra(idCliente).then(this.updateClientes
-                                   ).catch(this.errorDAO);
-            break;
-        default :
-            //default: action=="lista"
-            clientesDAO.buscaTodos().then(function (clientes) {
-                self.clientes=clientes;
-            }).catch(this.errorDAO);                       
-    };
+    updateClientes () {
+        this.$location.path("/lista");  //Post-Redirect-Get
+    }
 
- }]);  //ClientesRouteCtrl
+    errorDAO (response) {
+        this.errMsgs= response.data; //JAX-RS BeanValidation errors
+        console.log( "Error en servidor: " + response.status +" "+ response.statusText );
+    }
+
+    $onInit() {
+        //Init view model 
+        this.cliente = {};
+        this.clientes = [];
+        this.errMsgs = [];
+
+        //Process path actions
+        let idCliente = (this.$routeParams.idCliente ? parseInt(this.$routeParams.idCliente) : 0);
+        let action = "";
+        if (this.$location.path().length) {
+            //extract action name from path
+            action = this.$location.path().match(/^\/?(\w+)/)[1];
+        }
+
+        switch (action) {
+            case "visualiza":
+            case "edita":
+                this.clientesDAO.busca(idCliente)
+                        .then( cliente => this.cliente = cliente )
+                        .catch( response => this.errorDAO (response) );
+                break;
+            case "crea":
+                this.cliente = {};
+                break;
+            case "borra":
+                this.clientesDAO.borra(idCliente)
+                    .then(  () => this.updateClientes() )
+                    .catch( response => this.errorDAO(response) );
+                break;
+            default :
+                //default: action=="lista"
+                this.clientesDAO.buscaTodos()
+                        .then( clientes => this.clientes = clientes )
+                        .catch( response => this.errorDAO(response) );
+        }
+    }
+
+ };
+
+angular.module('clientesApp.controllers', [])
+  .controller('ClientesCtrl', ['ClientesDAOREST', ClientesCtrl ] )
+  .controller('ClientesRouteCtrl', ['$routeParams','$location','ClientesDAOREST',ClientesRouteCtrl]);
 
