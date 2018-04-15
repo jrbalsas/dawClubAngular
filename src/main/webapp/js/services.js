9698a2b4404280af22a0b0526ec829d1a81f6793
José Ramón Balsas <jrbalsas@ujaen.es>
2018-04-15 14:16:47 +0200

    ClientesDAOREST & ClientesDAOList migrated to ES6 class

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -2,127 +2,133 @@
 
 /* Services */
 
-angular.module('clientesApp.services', [])
-    .value('version', '0.1')
-    .service('ClientesDAOREST',['$http',function($http) {
+class ClientesDAOREST {
+    constructor($http) {
         //DAO REST implementation which return promises (Angular way)
-        var srvUrl="webservice/clientes";
+        this.$http = $http;
 
-        this.buscaTodos = function () {
+        this.srvUrl = "webservice/clientes";
+    }
+    buscaTodos() {
+        return this.$http.get(this.srvUrl).then(response => response.data);
+    }
 
-            return $http.get(srvUrl).then( function (response) {
-                    return response.data;
-                   });
-        };
+    busca(id) {
+        return this.$http.get(this.srvUrl + "/" + id).then(response => response.data);
+    }
 
-        this.busca = function (id) {
-                return $http.get(srvUrl+"/"+id).then (function (response) {
-                    return response.data;
-                });
-        };
-        
-        this.borra = function (id) {
-                  return $http.delete(srvUrl+"/"+id);
-        };
-        
-        this.guarda = function (cliente) {
-            return $http.put(srvUrl+"/"+cliente.id,cliente).then (function (response) {
-                return response.data;
-            });
-        };
+    borra(id) {
+        return this.$http.delete(this.srvUrl + "/" + id);
+    }
 
-        this.crea = function (cliente) {
-            return $http.post(srvUrl,cliente).then (function (response) {
-                return response.data;
-            });
-        };
+    guarda(cliente) {
+        return this.$http.put(this.srvUrl + "/" + cliente.id, cliente)
+                .then(response => response.data);
+    }
 
-    }])
-    .service('ClientesDAOList',['$q','$timeout',function($q,$timeout) {
+    crea(cliente) {
+        return this.$http.post(this.srvUrl, cliente).then(response => response.data);
+    }
+
+}
+
+class ClientesDAOList {
+    constructor($q, $timeout) {
         //DAO over simple array for testing purposes
         //modern DAO returning promises to attach user callbacks
         //Used for compatibility with DAO remote implementation methods
-                    
-        var idClientes=1;
-        var clientes= [
-            {id: idClientes++,
-             nombre: 'María González',
-             dni: '12345678-A',
-             socio: true},
-            {id: idClientes++,
-             nombre: 'Pedro Pérez',
-             dni: '87654321B',
-             socio: true},
-            {id: idClientes++,
-             nombre: 'Sergio Fuentes',
-             dni: '11223344',
-             socio: false}     
-        ];
+        this.$q=$q;
+        this.$timeout=$timeout;
 
-        //Simulate an async operation which returns data with some delay
-        //i.e $http()
-        function asyncOp (data) {
-            var deferred= $q.defer();  //Create promise
-            //create promise success method as in $http
+        this.idClientes = 1;
+        this.clientes = [
+            {id: this.idClientes++,
+                nombre: 'María González',
+                dni: '12345678-A',
+                socio: true},
+            {id: this.idClientes++,
+                nombre: 'Pedro Pérez',
+                dni: '87654321B',
+                socio: true},
+            {id: this.idClientes++,
+                nombre: 'Sergio Fuentes',
+                dni: '11223344',
+                socio: false}
+        ];
+    }
+    //Simulate an async operation which returns data with some delay
+    //i.e $http()
+    asyncOp(data) {
+        let deferred = this.$q.defer();  //Create promise
+        //create promise success method as in $http
 //            deferred.promise.success= function (usrCallBack) {
 //                deferred.promise.then(function(data){
 //                    usrCallBack(data);
 //                },
 //                null);
 //            };
-            $timeout(function() {
-                deferred.resolve(data);
-            }, 100);
-            return deferred.promise;
+        this.$timeout(function () {
+            deferred.resolve(data);
+        }, 100);
+        return deferred.promise;
+    }
+    //public methods
+    buscaTodos() {
+        return this.asyncOp(this.clientes);
+    }
+    busca(id) {
+        var cliente = {};
+        if (id > 0) {
+            //Look for cliente
+            this.clientes.some(function (c, key) {
+                if (c.id === id) {
+                    angular.copy(c, cliente);
+                    return true;
+                }
+                ;
+                return false;
+            });
         }
-        //public methods
-        this.buscaTodos= function () {                
-                return asyncOp(clientes);
-        };
-        this.busca=function (id) {
-                var cliente={};
-                if (id>0) {
-                  //Look for cliente
-                  clientes.some(function(c,key) {
-                      if (c.id===id) {
-                          angular.copy(c,cliente);
-                          return true;
-                      };
-                      return false;
-                  });
-              };
-              return asyncOp(cliente);
-        };
-        this.borra=function (id) {
-                if (id>0) {
-                  //Borrar cliente
-                  clientes.some(function(c,key) {
-                      if (c.id===id) {
-                          clientes.splice(key,1);
-                          return true;
-                      };
-                      return false;
-                  });
-              };
-              return asyncOp({});
-        };
-        this.guarda=function (cliente) {
-              if (cliente.id>0) {
-                //Modify cliente data
-                clientes.some(function(c,key) {
-                    if (c.id===cliente.id) {
-                        angular.copy(cliente,c);
-                        return true;
-                    };
-                    return false;
-                });
-              }
-              return asyncOp({});
-            };
-        this.crea=function(cliente) {
-                //New cliente
-                cliente.id=idClientes++;
-                clientes.push(cliente);
-                return asyncOp(cliente);
-            };         
-    }]);
+        ;
+        return this.asyncOp(cliente);
+    }
+    borra(id) {
+        if (id > 0) {
+            //Borrar cliente
+            this.clientes.some(function (c, key, clientes) {
+                if (c.id === id) {
+                    clientes.splice(key, 1);
+                    return true;
+                }
+                return false;
+            });
+        }
+        ;
+        return this.asyncOp({});
+    }
+    guarda(cliente) {
+        if (cliente.id > 0) {
+            //Modify cliente data
+            this.clientes.some(function (c, key) {
+                if (c.id === cliente.id) {
+                    angular.copy(cliente, c);
+                    return true;
+                }
+                ;
+                return false;
+            });
+        }
+        return this.asyncOp({});
+    }
+    crea(cliente) {
+        //New cliente
+        cliente.id = this.idClientes++;
+        this.clientes.push(cliente);
+        return this.asyncOp(cliente);
+    }
+}
+
+angular.module('clientesApp.services', [])
+        .value('version', '0.1')
+        .service('ClientesDAOList', ['$q', '$timeout', ClientesDAOList])
+        .service('ClientesDAOREST', ['$http', ClientesDAOREST]);
