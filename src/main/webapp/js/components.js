05ba02ddfb2fb86800844130411f4e18e19c14e2
José Ramón Balsas <jrbalsas@ujaen.es>
2018-04-17 20:37:00 +0200

    Added <errores> component

--- src/main/webapp/js/components.js
+++ src/main/webapp/js/components.js
@@ -1,20 +1,43 @@
 'use strict';
 
-/* Components */
+/* Component definition sample */
 
+/** Component controller */
 class ClienteComponentController {
     constructor () {
         
     }
 }
 
+/**Visualiza un resumen de los datos de un cliente
+   @notes use <cliente datos="objCliente"/>
+ */
+const clienteComponent = {
+    bindings: {
+        datos: '<' //Component attribute (single binding)
+    },
+    controller: ClienteComponentController,
+    template: '<p>{{$ctrl.datos.nombre}} ({{$ctrl.datos.dni}})</p>'
+};
+
+
+/**Visualiza un listado de errores de validación
+   @notes use <errores msgs="array_errores"/>
+ */
+const erroresComponent = {
+    bindings: {
+        msgs: '<' //Component attribute (single binding)
+    },
+    template: `
+    <div data-ng-show="$ctrl.msgs.length > 0">
+        <p class="text-danger" data-ng-repeat="err in $ctrl.msgs">
+            {{err.message}}
+        </p>
+    </div>    
+    `
+};
+
+//Angular components registration
 angular.module('clientesApp.components', [])
-    .component('cliente', {
-            //Visualiza un resumen de los datos de un cliente
-            //Uso: <cliente datos="objCliente"/>
-            bindings: {
-                datos: '<'
-            },
-            controller: ClienteComponentController,
-            template: '<p>{{$ctrl.datos.nombre}} ({{$ctrl.datos.dni}})</p>'
-        });
+    .component('cliente', clienteComponent)
+    .component('errores', erroresComponent);
